5a600b5fa1dd0e513427e26007961d86e50259b7
Benjamin Padgette <benjamin.padgette@gmail.com>
2025-01-23 22:25:46 +0100

    fix(examples): use pre until serverside markdown rendering

--- examples/docs.c
+++ examples/docs.c
@@ -3,8 +3,9 @@
 #define PAGE_TITLE "Ozone Documentation"
 
 int homepage(OzoneAppEvent* event, OzoneAppContext* context) {
-  OzoneStringVector readme_md = (OzoneStringVector) { 0 };
+  OzoneStringVector readme_md = ozoneVectorFromElements(OzoneString, ozoneStringConstant("<pre>\n"));
   ozoneFileLoadFromPath(event->allocator, &readme_md, &ozoneStringConstant("./README.md"), 1024);
+  ozoneVectorPushOzoneString(event->allocator, &readme_md, &ozoneStringConstant("\n</pre>"));
 
   ozoneAppRenderOzoneShellHTML(
       event, context, &ozoneStringConstant(PAGE_TITLE), ozoneStringJoin(event->allocator, &readme_md));
