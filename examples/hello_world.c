5d638d2abb3381e4c674d5b2e99208530f9d1875
Benjamin Padgette <benjamin.padgette@gmail.com>
2025-02-10 22:00:18 +0100

    feat(examples): update hello_world

--- examples/hello_world.c
+++ examples/hello_world.c
@@ -5,7 +5,11 @@
 #define PAGE_TITLE "My Ozone Server"
 
 void setHeaders(OzoneAppEvent* event) {
-  ozoneAppSetResponseHeader(event, &ozoneStringConstant("X-Server-Name"), &ozoneStringConstant(PAGE_TITLE));
+  ozoneMapInsertOzoneString(
+      event->allocator,
+      &event->response->headers,
+      &ozoneStringConstant("X-Server-Name"),
+      &ozoneStringConstant(PAGE_TITLE));
 }
 
 void handleBadRequest(OzoneAppEvent* event) {
@@ -19,12 +23,24 @@ void home(OzoneAppEvent* event) {
   event->response->body = *ozoneString(
       event->allocator,
       "<h1>ozone</h1>"
-      "<p>Ozone is a minimal dependency, C and web standards based web framework.</p>");
+      "<p>Ozone is a minimal dependency, C and web standards based web framework.</p>"
+      "<a href=\"/greet/World\">/greet/World</a></li>");
 }
 
-void renderHTTPStatusToBody(OzoneAppEvent* event) {
-  // ozone modules like ozone_http expose helpful functions too
-  event->response->body = *ozoneHTTPStatusText(event->allocator, event->response->code);
+void greet(OzoneAppEvent* event) {
+  event->response->body = *ozoneString(event->allocator, "<a href=\"/\">Home</a><h1>Hello, ");
+
+  OzoneString* name = ozoneMapGetOzoneString(&event->parameters, &ozoneStringConstant("path:nickname"));
+  if (!name) {
+    name = ozoneMapGetOzoneString(&event->parameters, &ozoneStringConstant("path:name"));
+  }
+
+  if (!name) {
+    name = &ozoneStringConstant("World");
+  }
+
+  ozoneStringConcatenate(event->allocator, &event->response->body, name);
+  ozoneStringConcatenate(event->allocator, &event->response->body, &ozoneStringConstant("!</h1>"));
 }
 
 int main(int argc, char* argv[]) {
@@ -32,7 +48,9 @@ int main(int argc, char* argv[]) {
       OzoneAppEndpoint,
       // Endpoints can take an arbitrary count of handlers
       ozoneAppEndpoint(GET, "/", setHeaders, home, asHTMLDocument),
-      ozoneAppEndpoint(POST, "/use-javascript", setHeaders, handleBadRequest, renderHTTPStatusToBody, asHTMLDocument),
+      ozoneAppEndpoint(GET, "/greet/{name}", setHeaders, greet, asHTMLDocument),
+      ozoneAppEndpoint(GET, "/greet/{name}/secret/{nickname}", setHeaders, greet, asHTMLDocument),
+      ozoneAppEndpoint(POST, "/use-javascript", setHeaders, handleBadRequest, asHTMLDocument),
       ozoneAppEndpoint(PUT, "/just-write-it-in-rust", setHeaders, handleBadRequest));
 
   return ozoneAppServe(argc, argv, &endpoints);
