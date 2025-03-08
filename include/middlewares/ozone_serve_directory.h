3290de497a77db9a2eaab4e08b5b024a1f8d232b
Benjamin Padgette <benjamin.padgette@gmail.com>
2025-03-09 00:57:48 +0100

    feat(http): content types for file serving

--- include/middlewares/ozone_serve_directory.h
+++ include/middlewares/ozone_serve_directory.h
@@ -2,6 +2,9 @@
 #define OZONE_SERVE_DIRECTORY_H
 #include "ozone.h"
 
+#define OZONE_SERVE_DIRECTORY_CONTENT_TYPES "middlewares:serve-directory:content-types"
+#define OZONE_SERVE_DIRECTORY_DEFAULT_CONTENT_TYPE "application/octet-stream"
+
 void ozoneServeDirectory(OzoneAppEvent* event) {
   OzoneString* location = ozoneAppParameter(event, "path:location");
   if (!location) {
@@ -12,15 +15,16 @@ void ozoneServeDirectory(OzoneAppEvent* event) {
 
   ozoneAppContextLock(event);
   OzoneString** directory = ozoneAppContextCacheGetRef(event, OzoneString, "option:serve-directory");
-  ozoneAppContextUnlock(event);
-
   if (!directory) {
+    ozoneAppContextUnlock(event);
     ozoneLogError("CLI option serve-directory not specified");
     event->response->code = 503;
     return;
   }
 
   OzoneString* path = ozoneStringCopy(event->allocator, *directory);
+  ozoneAppContextUnlock(event);
+
   ozoneStringWriteByte(event->allocator, path, '/');
   ozoneStringConcatenate(event->allocator, path, location);
 
@@ -31,9 +35,47 @@ void ozoneServeDirectory(OzoneAppEvent* event) {
     return;
   }
 
+  // TODO: efficiently chunk HTTP responses
   ozoneStringVectorConcatenate(event->allocator, &event->response->body, &file_chunks);
 
-  // TODO: infer Content-Type & chunk HTTP responses
+  int extension_start = 1 + ozoneStringFindLast(path, &ozoneString("."));
+  OzoneString* extension = ozoneStringAllocate(event->allocator, "");
+  while (extension_start < (int)ozoneStringLength(path)) {
+    ozoneStringWriteByte(event->allocator, extension, ozoneStringBufferAt(path, extension_start));
+    extension_start++;
+  }
+
+  OzoneString* content_type = ozoneStringAllocate(event->allocator, OZONE_SERVE_DIRECTORY_DEFAULT_CONTENT_TYPE);
+  OzoneStringMapInsert(event->allocator, &event->response->headers, &ozoneString("Content-Type"), content_type);
+
+  ozoneAppContextLock(event);
+  OzoneString** content_types_path = ozoneAppContextCacheGetRef(event, OzoneString, "option:content-types");
+  if (!content_types_path) {
+    ozoneAppContextUnlock(event);
+    ozoneLogWarn("CLI option content-types not specified, all files will be "
+                 "assigned " OZONE_SERVE_DIRECTORY_DEFAULT_CONTENT_TYPE);
+    return;
+  }
+
+  OzoneStringMap** content_types
+      = ozoneAppContextCacheGetRef(event, OzoneStringMap, OZONE_SERVE_DIRECTORY_CONTENT_TYPES);
+  if (!content_types) {
+    OzoneStringMap* content_types_map = ozoneAllocatorReserveOne(event->context->allocator, OzoneStringMap);
+    ozonePropertiesLoadFromPath(event->context->allocator, content_types_map, *content_types_path);
+    content_types = &content_types_map;
+
+    OzoneAppVoidRefMapInsert(
+        event->context->allocator,
+        event->context->cache,
+        &ozoneString(OZONE_SERVE_DIRECTORY_CONTENT_TYPES),
+        (OzoneAppVoidRef*)content_types);
+  }
+
+  content_type = OzoneStringMapFind(*content_types, extension);
+  if (content_type)
+    OzoneStringMapInsert(event->allocator, &event->response->headers, &ozoneString("Content-Type"), content_type);
+
+  ozoneAppContextUnlock(event);
 }
 
 #endif
